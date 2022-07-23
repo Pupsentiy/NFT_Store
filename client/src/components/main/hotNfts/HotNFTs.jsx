fb33dea2ac92c454beb805b73adba68540dd8022
Alexander <pupsentiy@gmail.com>
2022-07-23 19:35:40 +0300

    d

--- client/src/components/main/hotNfts/HotNFTs.jsx
+++ client/src/components/main/hotNfts/HotNFTs.jsx
@@ -16,6 +16,12 @@ const HotNFTs = () => {
                     <button className="hotNFTs__btn-nav">Videos</button>
                     <button className="hotNFTs__btn-nav">More</button>
                 </nav>
+
+                <section className="hotNFTs__block-createNft">
+                  <h2 className="hotNFTs__title-createNft">Create Your Own NFT!</h2>
+                  <p className="hotNFTs__discription">We have a large scale group to support each other in this game, Join us to get the news as soon as possible and follow our latest announcements!</p>
+                  <button className="hotNFTs__creat-btn">Join Comunity Now</button>
+                </section>
         </div>
     </div>
   )
