126af5a1471452ab2672adddfaef9c83506d07d4
Alexander <pupsentiy@gmail.com>
2022-09-08 12:24:18 +0300

    попытка написать бэкенд + бд postgres, для авторизации

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "./components/auth/Auth";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
@@ -7,21 +7,16 @@ import routesConfig from "./routes/routesConfig";
 
 function App() {
   return (
-    <BrowserRouter  >
-     <Header/>
-     <Auth/>
-     <Routes>
-      {routesConfig.map((route, index) => (
-        <Route 
-        key={index}
-        path={route.path}
-        element={route.element}
-        />
-      ))}
-     </Routes>
-     <Footer/>
+    <BrowserRouter>
+      <Header />
+      <Auth />
+      <Routes>
+        {routesConfig.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
-
 export default App;
---
+++ server/models/models.js
@@ -0,0 +1,51 @@
+const sequelize = require("./db");
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define("user", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: "USER" },
+});
+
+const Basket = sequelize.define("basket", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const BasketCard = sequelize.define("basket_card", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const Card = sequelize.define("card", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+  price: { type: DataTypes.INTEGER, allowNull: false },
+  autor: { type: DataTypes.STRING, unique: true, allowNull: false },
+  img: { type: DataTypes.STRING, allowNull: false },
+  avatar: { type: DataTypes.STRING, allowNull: false },
+  category: { type: DataTypes.INTEGER, allowNull: false },
+});
+
+const Category = sequelize.define("category", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+User.hasOne(Basket);
+Basket.belongsTo(User);
+
+Category.hasMany(Card);
+Card.belongsTo(Category);
+
+Card.hasMany(BasketCard);
+BasketCard.belongsTo(Card);
+
+Basket.hasMany(BasketCard);
+BasketCard.belongsTo(Basket);
+
+module.exports = {
+  User,
+  Basket,
+  BasketCard,
+  Category,
+};
